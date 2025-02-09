fbcf610df5f5804740d3649d7ec1ac051693b8bc
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-02-09 14:34:14 -0800

    Time: 3 ms (22.08%), Space: 17.8 MB (46.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        opening = ["(","{","["]
+        closing = [")","}","]"]
+        length = len(s)
+        for i in range(length):
+            if s[i] in opening:
+                stack.append(s[i])
+            if s[i] in closing:
+                if len(stack)==0:
+                    return False
+                find = stack[-1]
+                if opening.index(find)!=closing.index(s[i]):
+                    return False
+                else:
+                    stack.pop()
+        return len(stack)==0
+        
