bcf1fd6cb2d1bd5a3dd5bc3ab3157c810fdcda72
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-04-02 19:03:02 -0700

    Time: 3 ms (88.92%), Space: 19.1 MB (62.25%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        ##BFS 
+        graph = [[] for _ in range(numCourses)]
+        inorder = [0] * numCourses
+
+        print(graph)
+
+        for node, prerequisite in prerequisites:
+            graph[prerequisite].append(node)
+            inorder[node] +=1
+        
+
+        q = deque([i for i in range(numCourses) if inorder[i]==0])
+        count = 0
+
+        while q:
+            course = q.popleft()
+            count +=1
+
+            for nei in graph[course]:
+                inorder[nei] -=1
+                if inorder[nei] ==0:
+                    q.append(nei)
+
+        return count==numCourses
+
+
+
+
+        
+        
