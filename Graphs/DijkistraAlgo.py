1b3273442eede260728a7961661e91261a8f58f8
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-04-10 16:38:24 -0700

    refactoring

--- Graphs/DijkistraAlgo.py
+++ Graphs/DijkistraAlgo.py
@@ -1,29 +1,37 @@
 import heapq
 from collections import defaultdict
 
-def networkDelayTime(times, n, k):
-    adj_list = defaultdict(list)
+def dijkstra(graph, n, start):
+    adj_set = defaultdict(list)
+    for u,v,w in graph:
+        adj_set[u].append((w,v))
+    dist = [float('inf')] * (n+1)
+    dist[start] = 0
+    heap = [(0,start)]
 
-    for x,y,w in times:
-        adj_list[x].append((w, y))
-
-    visited=set()
-    heap = [(0, k)]
     while heap:
-        travel_time, node = heapq.heappop(heap)
-        visited.add(node)
+        w1,n1 = heapq.heappop(heap)
+        if w1 > dist[n1]:
+            continue
+        for w2,n2 in adj_set[n1]:
+            if dist[n2]> w2+w1:
+                dist[n2] = min(dist[n2], w1+w2)
+                heapq.heappush(heap,(dist[n2],n2))
 
-        if len(visited)==n:
-            return travel_time
+    return dist[1:]
 
-        for time, adjacent_node in adj_list[node]:
-            if adjacent_node not in visited:
-                heapq.heappush(heap, (travel_time+time, adjacent_node))
-
-    return -1
-
-if __name__=='__main__':
-    times = [[2,1,1],[2,3,1],[3,4,1]]
-    n = 4
-    k = 2
-    print(networkDelayTime(times, n, k))
+if __name__ == '__main__':
+    graph = [
+        (1, 2, 2),
+        (1, 3, 4),
+        (2, 3, 1),
+        (2, 4, 7),
+        (3, 5, 3),
+        (4, 6, 1),
+        (5, 4, 2),
+        (5, 6, 5)
+    ]
+    n = 6
+    start = 1
+    distances = dijkstra(graph, n, start)
+    print("Shortest distances from node", start, ":", distances)
---
+++ Graphs/lc743_DijkistraAlgo.py
@@ -0,0 +1,29 @@
+import heapq
+from collections import defaultdict
+
+def networkDelayTime(times, n, k):
+    adj_list = defaultdict(list)
+
+    for x,y,w in times:
+        adj_list[x].append((w, y))
+
+    visited=set()
+    heap = [(0, k)]
+    while heap:
+        travel_time, node = heapq.heappop(heap)
+        visited.add(node)
+
+        if len(visited)==n:
+            return travel_time
+
+        for time, adjacent_node in adj_list[node]:
+            if adjacent_node not in visited:
+                heapq.heappush(heap, (travel_time+time, adjacent_node))
+
+    return -1
+
+if __name__=='__main__':
+    times = [[2,1,1],[2,3,1],[3,4,1]]
+    n = 4
+    k = 2
+    print(networkDelayTime(times, n, k))
