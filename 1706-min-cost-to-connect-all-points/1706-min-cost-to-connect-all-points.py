7b8ed5d5da7468ae6d67cae3d507d1cb04bee1a9
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-04-11 19:08:29 -0700

    Time: 3067 ms (16.68%), Space: 153.4 MB (30.5%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -1,32 +1,32 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        if n <= 1:
+        if n<2:
             return 0
-        visited = [0] * n
-        sum = 0
+
+        def ManhattanDist(p1,p2):
+            return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+
         adj_list = [[] for _ in range(n)]
+        sum = 0
+        visited = [False] * n
+        # weight, node
+        minHeap = [(0,0)]
 
-        def getmanHattanDist(p1,p2):
-            return abs(p1[0] - p2[0]) + abs(p2[1]-p1[1])
-        
         for i in range(n):
-            for j in range(i + 1, n):
-                distance = getmanHattanDist(points[i], points[j])
-                adj_list[i].append((j, distance))
-                adj_list[j].append((i, distance))
-        
-        size = len(adj_list)
-        
-        # w,destnode
-        heap = [(0,0)]
+            for j in range(i+1,n):
+                dist = ManhattanDist(points[i],points[j])
+                adj_list[i].append((dist, j))
+                adj_list[j].append((dist,i))
 
-        while heap:
-            w1,n1 = heapq.heappop(heap)
+        while minHeap:
+            w1,n1 = heapq.heappop(minHeap)
             if not visited[n1]:
-                visited[n1]=1
+                visited[n1] = True
                 sum +=w1
-                for n2,w2 in adj_list[n1]:
-                    heapq.heappush(heap,(w2,n2))
+                for w2,n2 in adj_list[n1]:
+                    if not visited[n2]:
+                        heapq.heappush(minHeap,(w2,n2))
+
         return sum
-    
+        
