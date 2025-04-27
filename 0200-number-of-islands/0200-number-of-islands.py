8b09e99b3b95c2dac093d6ad845f08e70ce3f8a5
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-04-27 15:37:39 -0700

    Time: 263 ms (39.11%), Space: 20.8 MB (37.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
         rows = len(grid)
         cols = len(grid[0])
-        visited = [[0 for _ in range(cols)]for _ in range(rows)]
-        count =0
+        visited = [[0 for _ in range(cols)] for _ in range(rows)]
+        count = 0
 
-        def isSafe(newRow,newCol):
-            return 0<=newRow<rows and 0<=newCol<cols and grid[newRow][newCol]=="1" and visited[newRow][newCol]==0
+        def isValid(newRow,newCol):
+            return 0<=newRow<rows and 0<=newCol<cols and visited[newRow][newCol]==0 and grid[newRow][newCol]=='1'
+
 
         def DFS(row,col):
-            visited[row][col] = 1
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            directions = [(1,0),(-1,0),(0,1),(0,-1)]
+            visited[row][col]=1
 
-            for rx,cy in directions:
-                newRow = row + rx
-                newCol = col + cy
+            for dx,dy in directions:
+                newRow = dx + row
+                newCol = dy + col
 
-                if isSafe(newRow, newCol):
+                if isValid(newRow,newCol):
                     DFS(newRow,newCol)
+
+
 
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == '1' and visited[i][j] == 0:
+                if grid[i][j]=='1' and visited[i][j]==0:
                     count+=1
                     DFS(i,j)
-        
         return count
+
+        
